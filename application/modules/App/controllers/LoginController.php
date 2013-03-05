8c50c5e60a51a99dd099ed3205ef0ec2450c0969
Benjamin Joseph Vickers <benjaminjosephvickers@Catfish.local>
2013-03-05 06:41:56 +0000

    Hardened the Login software against user error.

--- application/modules/App/controllers/LoginController.php
+++ application/modules/App/controllers/LoginController.php
@@ -4,24 +4,101 @@ class App_LoginController extends Zend_Controller_Action {
 
     public function init() {
     	
-        /* Initialize action controller here */
+        //If action is index and user is not logged in, allow
+        if($this->getRequest()->getActionName() == 'index') {
+        	
+        	if(Zend_Auth::getInstance()->hasIdentity()) {
+        		
+        		$session = new Zend_Session_Namespace('already_identified');
+        		$session->isAuthorisedToShowAlreadyIdentified = true;
+        		$redirector = $this->_helper->getHelper('Redirector');
+        		$redirector->gotoRoute(
+        			array(
+        				'action' => 'already-identified',
+        				'controller' => 'Login',
+        				'module' => 'App'
+        			),
+        			'module_full_path',
+        			true
+        		);
+        	}
+        	
+        	//If here then user is not logged in and has tried to access the login page.
+        	//This is permissable.
+        	return;
+        }
+        
+    	
+        //If action is log-out, clear session and logout, regardless of whether the user
+    	//is logged in or not.
+    	if($this->getRequest()->getActionName() == 'logout') {
+    		
+	        Zend_Auth::getInstance()->clearIdentity();
+	        $redirector = $this->_helper->getHelper('Redirector');
+	        $redirector->gotoRoute(
+	        	array(
+	        		'action'=> 'index',
+	        		'controller' => 'Login',
+	        		'module' => 'App'
+	        	),
+	        	'module_partial_path',
+	        	true
+	        );
+    	}
+        
+
+    	if($this->getRequest()->getActionName() == 'already-identified') {
+    	
+    		$session = new Zend_Session_Namespace('already_identified');
+    		$isAuthorisedToShowAlreadyIdentified = $session->isAuthorisedToShowAlreadyIdentified;
+    		Zend_Session::namespaceUnset('already_identified');
+    		
+    		if(Zend_Auth::getInstance()->hasIdentity()) {
+    			
+    			if(empty($isAuthorisedToShowAlreadyIdentified)) {
+    				
+    				//Logged in but not previously authorised to see this page (i.e. the user
+    				//has tried to access this page directly rather than indirectly through the
+    				//Login page when already logged in. Send the user to the App Home rather
+    				//than error page, as they may just have refreshed the screen.
+    				$redirector = $this->_helper->getHelper('Redirector');
+					$redirector->gotoRoute(
+						array(
+							'action' => 'index', 
+							'controller' => 'Employee', 
+							'module' => 'App'
+						),
+						'module_partial_path',
+						true
+					);
+    			}
+    		}
+    		else {
+    			
+    			//Not logged in so should not see this page. Redirect to login.
+    			$redirector = $this->_helper->getHelper('Redirector');
+		        $redirector->gotoRoute(
+		        	array(
+		        		'action'=> 'index',
+		        		'controller' => 'Login',
+		        		'module' => 'App'
+		        	),
+		        	'module_partial_path',
+		        	true
+		        );
+    		}
+    	}
     }
 
+    /**
+     * @todo
+     * Track number of login attempts and lock account after several attempts. Possibly
+     * block IP for a while.
+     * 
+     * @todo
+     * Add security certificate
+     */
     public function indexAction() {
-    	
-    	if(Zend_Auth::getInstance()->hasIdentity()) {
-    		
-    		$helper = $this->_helper->getHelper('Redirector');
-        	$helper->gotoRoute(
-        		array(
-        			'action' => 'already-identified', 
-        			'controller' => 'Login', 
-        			'module' => 'App'
-        		),
-        		'module_full_path',
-        		true
-        	);
-        }
         
         $form = new App_Form_Login();
 		$request = $this->getRequest();
@@ -77,23 +154,13 @@ class App_LoginController extends Zend_Controller_Action {
     }
 
     public function alreadyIdentifiedAction() {
-        // action body
     }
 
+    /**
+     * @todo
+     * Delete the corresponding view to this action, as it is not used.
+     */
     public function logoutAction() {
-    	
-        // action body
-        Zend_Auth::getInstance()->clearIdentity();
-        $helper = $this->_helper->getHelper('Redirector');
-        $helper->gotoRoute(
-        	array(
-        		'action'=> 'index', 
-        		'controller' => 'Login', 
-        		'module' => 'App'
-        	),
-        	'module_partial_path',
-        	true
-        );
     }
 }
 
